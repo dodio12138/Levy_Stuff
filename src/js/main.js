63feaa2e3d746ad70774afaff57f7f97d40c6541
dodio12138 <1159715241@qq.com>
2023-04-13 23:53:23 +0100

    stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 var BunnyGame = document.getElementById('BunnyGame');
 
+var stuff = document.getElementById('Stuff');
+
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 BunnyGame.addEventListener('dblclick', function(event) {
@@ -10,6 +12,22 @@ BunnyGame.onclick = function(event) {
     if(isMobile)
     {
         //window.open(BunnyGame.href, '_blank')
+    }
+    else
+    {
+        event.preventDefault(); // 阻止默认行为
+        return false; // 取消事件传播
+    }
+};
+
+stuff.addEventListener('dblclick', function(event) {
+    //event.preventDefault();
+    window.open(stuff.href, '_blank'); // 在新窗口中打开链接
+  });
+  stuff.onclick = function(event) {
+    if(isMobile)
+    {
+        //window.open(stuff.href, '_blank')
     }
     else
     {
