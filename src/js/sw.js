c1325b750db9bc9cd1ec15be3fae0c1305458f03
dodio12138 <1159715241@qq.com>
2023-04-23 15:55:36 +0100

    sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,41 +1,33 @@
-function handleRegistration(registration){
-    console.log('Service Worker Registered. ', registration)
-    registration.onupdatefound = (e) => {
-      const installingWorker = registration.installing;
-      installingWorker.onstatechange = (e) => {
-        if (installingWorker.state !== 'installed') return;
-        if (navigator.serviceWorker.controller) {
-          console.log('SW is updated');
-        } else {
-          console.log('A Visit without previous SW');
-          createSnackbar({
-            message: 'App ready for offline use.',
-            duration: 3000
-          })
+//缓存空间名称
+var CACHE_VERSION = 'sw_v1';
+//需缓存的文件
+var CACHE_FILES = [
+  '/',
+  '/aaa.html',
+  '/bbb.jpg'
+];
+
+//监听安装事件
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+    .then(cache => cache.addAll(CACHE_FILES)
+      .then(() => self.skipWaiting())
+    ));
+});
+
+//监听激活事件
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key, i) {
+        if (key !== CACHE_VERSION) {
+          return caches.delete(keys[i]);
         }
-      };
-    }
-  }
-  
-  if(navigator.serviceWorker){
-    
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => handleRegistration(registration))
-      .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
-  
-    
-    navigator.serviceWorker.onmessage = (e) => {
-      console.log('SW: SW Broadcasting:', event);
-      const data = e.data
-      
-      if(data.command == "UPDATE_FOUND"){
-        console.log("UPDATE_FOUND_BY_SW", data);
-        createSnackbar({
-          message: "Content updated.",
-          actionText:"refresh",
-          action: function(e){location.reload()}
-        })
-      }
-    }
-  }
+      }));
+    })
+  );
+});
+
+//不缓存策略
+self.addEventListener('fetch', function(event) {});
