260509e11e0d31f42924c0f6c3ae1e06d1e7fb61
dodio12138 <1159715241@qq.com>
2024-01-15 20:57:09 +0000

    taskbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,72 +1,64 @@
+function time() {
+    var date = new Date();
+    hours = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    if (sec<10){
+        document.getElementById('taskbar-time').innerHTML = hours + ':' + min + ':0' + sec;
+    }else{
+        document.getElementById('taskbar-time').innerHTML = hours + ':' + min + ':' + sec;
+    }
+    
+}
+setInterval("time()", 500);
+
 var BunnyGame = document.getElementById('BunnyGame');
 
 var stuff = document.getElementById('Stuff');
 
 var FunPages = document.getElementById('FunPages');
 
-var Meterminal = document.getElementById('Meterminal');
+var MeTerminal = document.getElementById('MeTerminal');
 
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-BunnyGame.addEventListener('dblclick', function(event) {
+BunnyGame.addEventListener('dblclick', function (event) {
     //event.preventDefault();
     window.open(BunnyGame.href, '_blank'); // 在新窗口中打开链接
-  });
-BunnyGame.onclick = function(event) {
-    if(isMobile)
-    {
+});
+BunnyGame.onclick = function (event) {
+    if (isMobile) {
         //window.open(BunnyGame.href, '_blank')
     }
-    else
-    {
+    else {
         event.preventDefault(); // 阻止默认行为
         return false; // 取消事件传播
     }
 };
 
-FunPages.addEventListener('dblclick', function(event) {
+FunPages.addEventListener('dblclick', function (event) {
     //event.preventDefault();
     window.open(FunPages.href, '_blank'); // 在新窗口中打开链接
-  });
-  FunPages.onclick = function(event) {
-    if(isMobile)
-    {
+});
+FunPages.onclick = function (event) {
+    if (isMobile) {
         //window.open(BunnyGame.href, '_blank')
     }
-    else
-    {
+    else {
         event.preventDefault(); // 阻止默认行为
         return false; // 取消事件传播
     }
 };
 
-stuff.addEventListener('dblclick', function(event) {
+MeTerminal.addEventListener('dblclick', function (event) {
     //event.preventDefault();
-    window.open(stuff.href, '_blank'); // 在新窗口中打开链接
-  });
-  stuff.onclick = function(event) {
-    if(isMobile)
-    {
-        //window.open(stuff.href, '_blank')
+    window.open(MeTerminal.href, '_blank'); // 在新窗口中打开链接
+});
+MeTerminal.onclick = function (event) {
+    if (isMobile) {
+        //window.open(MeTerminal.href, '_blank')
     }
-    else
-    {
-        event.preventDefault(); // 阻止默认行为
-        return false; // 取消事件传播
-    }
-};
-
-Meterminal.addEventListener('dblclick', function(event) {
-    //event.preventDefault();
-    window.open(Meterminal.href, '_blank'); // 在新窗口中打开链接
-  });
-  Meterminal.onclick = function(event) {
-    if(isMobile)
-    {
-        //window.open(Meterminal.href, '_blank')
-    }
-    else
-    {
+    else {
         event.preventDefault(); // 阻止默认行为
         return false; // 取消事件传播
     }
